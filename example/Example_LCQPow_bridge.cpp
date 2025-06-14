71ab9db9a66e65f10e04d7251dc87c7818afbbf5
Haowen Yao <haowen.yao@tum.de>
2025-06-14 21:43:15 +0200

    update the LICENSE before making repository public.

--- example/Example_LCQPow_bridge.cpp
+++ example/Example_LCQPow_bridge.cpp
@@ -1,6 +1,20 @@
 /**
  * @example Example_LCQPow_bridge.cpp
  * @brief Example of using the LCQPow_bridge to solve a linear complementarity problem.
+ *  Copyright (C) 2025 Haowen Yao
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include<FLIQC_controller_core/LCQPow_bridge.hpp>
 #include<cmath>
--- src/LCQPow_bridge.cpp
+++ src/LCQPow_bridge.cpp
@@ -1,6 +1,20 @@
 /**
  * @file LCQPow_bridge.cpp
  * @brief Implementation of the LCQPow_bridge class.
+ *  Copyright (C) 2025 Haowen Yao
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "FLIQC_controller_core/LCQPow_bridge.hpp"
 #include <memory>
